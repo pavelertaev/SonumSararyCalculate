d12e9bded984504d84304897c86445ca8426fe60
паша <pashka_ertaev@mail.ru>
2023-01-28 14:39:39 +0300

    Initial commit

---
+++ src/main/java/work/sonum/sonumsararycalculate/SonumSararyCalculateApplication.java
@@ -0,0 +1,13 @@
+package work.sonum.sonumsararycalculate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonumSararyCalculateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonumSararyCalculateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/sonum/sonumsararycalculate/SonumSararyCalculateApplicationTests.java
@@ -0,0 +1,13 @@
+package work.sonum.sonumsararycalculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonumSararyCalculateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
